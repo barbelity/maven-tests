2e0c10a2ce0cbdba1d339511fdb6c351a9852e26
barbelity <barb@jfrog.com>
2018-12-26 17:02:09 +0200

    Adding classes

---
+++ src/main/java/com/barbelity/App.java
@@ -0,0 +1,11 @@
+package com.barbelity;
+
+/**
+ * Created by Bar Belity on 26/12/2018.
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
